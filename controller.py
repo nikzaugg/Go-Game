359630d04941cc6eda2b626af2232e4bcf412a9c
Nik Zaugg <zauggnik@gmail.com>
2017-11-18 11:30:50 +0100

    remove dummy classes

--- controller.py
+++ controller.py
@@ -7,17 +7,17 @@
 
 import pyglet
 from game_model import Model
-from client import Window
+import client
 
 class Controller(object):
     def __init__(self):
-        self.window = DummyWindow()
+        self.window = client.Window()
         self.model = Model(19)
         self.data = self.model.get_data()
         self.window.receive_data(self.data)
         
         # TODO: Do we run the app here as well?
-        #pyglet.app.run()
+        # pyglet.app.run()
     
     def _update_window(self):
         self.window.receive_data(self.model.get_data())
@@ -43,49 +43,6 @@ class Controller(object):
         # TODO: remove comment once model has implemented Additional Task 2: Mark territory
         # self.model.mark_territory(pos.x, pos.y)
         self._update_window()
-        
-
-######### DUMMY WINDOW VIEW ###########
-# TODO: remove once view is ready
-class DummyWindow(object):
-    def __init__(self):
-        self.info = self.Label()
-
-    def receive_data(self, data):
-        pass
-
-    class Label():
-        def __init__(self):
-            self.text = ''
-########################################
 
 if __name__ == '__main__':
     c = Controller()
-
-
-''' class DummyModel(object):
-    def __init__(self,n=19):
-        self.data = {   'size' : n,
-                        'stones' : [[None for i in range(n)]for j in range(n)],
-                        'territory': [[None for i in range(n)]for j in range(n)],
-                        'game_over': False,
-                        'score' : (0,0),
-                        'color' : True} 
-
-    def find_territory(self):
-        pass
-
-    def get_data(self):
-        return self.data
-
-    def mark_territory(self,x, y):
-        pass
-
-    def passing(self):
-        return True
-
-    def place_stone(self, x, y):
-        return False
-
-    def play(self, pos):
-        return True '''
